5e7146656f8d771894b8133379729aadc6fffd89
Ziyan <jaraidaniel@gmail.com>
2017-09-24 16:31:59 +0200

    Disable dexpreopt
    Saves precious space on /system, needed for gapps, at the cost of a
    longer initial boot time.
    
    Change-Id: Ia39a700860eac912f90523f04f77843e254cd631

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,15 +88,8 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
+# Disable dex-preoptimization
+WITH_DEXPREOPT := false
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
