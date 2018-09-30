45ad4654310ee51ff65903f689d568053975df22
nitin1438 <nitin.chobhe@gmail.com>
2018-09-30 17:30:27 +0000

    Revert "mako: Add basic USB HAL that reports no status change"
    This reverts commit a394adf39a2c885d4ca87c3c7ee14bac987dec29.

--- device.mk
+++ device.mk
@@ -75,6 +75,10 @@ PRODUCT_PACKAGES += \
     libbt-vendor \
     android.hardware.bluetooth@1.0-impl
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
